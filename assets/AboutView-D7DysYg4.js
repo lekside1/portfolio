a3d725c0060dad0cdfde3c6c6e5400be571cf75a
faruq <lekside1@gmail.com>
2024-12-19 22:16:05 -0500

    Updates

---
+++ assets/AboutView-D7DysYg4.js
@@ -0,0 +1,1 @@
+import{P as e}from"./PageComponent-CWfXTOcD.js";import{d as o,c as t,o as a}from"./index-B9OTjYvQ.js";const m=o({__name:"AboutView",setup(n){return(r,c)=>(a(),t(e,{title:"About"}))}});export{m as default};
---
+++ assets/PortfolioView-ByLhK6jd.js
@@ -0,0 +1,1 @@
+import{P as o}from"./PageComponent-CWfXTOcD.js";import{d as e,c as t,o as r}from"./index-B9OTjYvQ.js";const i=e({__name:"PortfolioView",setup(a){return(n,c)=>(r(),t(o,{title:"Portfolio"}))}});export{i as default};
