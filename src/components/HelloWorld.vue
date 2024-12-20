3631d035ef6eed4076c9656e5c0c397cbe5ab2f1
faruq <lekside1@gmail.com>
2024-12-19 21:55:55 -0500

    add deploy command to package json, update Hello world message

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
+      Hello World to my Vite-powered Vue 3 app.
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
