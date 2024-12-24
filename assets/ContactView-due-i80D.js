5e64e649ec13611cd9f7fba609bdc687194db0ac
faruq <lekside1@gmail.com>
2024-12-23 19:59:10 -0500

    Updates

---
+++ assets/ContactView-due-i80D.js
@@ -0,0 +1,1 @@
+import{P as t}from"./PageComponent-qesVkMW0.js";import{d as e,c as o,o as a}from"./index-Ck8RioZt.js";const m=e({__name:"ContactView",setup(n){return(c,r)=>(a(),o(t,{title:"Contact"}))}});export{m as default};
