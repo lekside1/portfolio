d7e2cc99b822663bdfa39871f43f7f9dd6f47bed
faruq <lekside1@gmail.com>
2025-01-06 03:18:41 -0500

    update pagecomponent to pageContent, add TitleComponent

--- src/components/PageContent.vue
+++ src/components/PageContent.vue
@@ -1,21 +1,16 @@
 <script setup lang="ts">
-defineProps<{
-  title?: string
-  content?: string
-}>()
+// defineProps<{
+//   content?: string
+// }>()
 </script>
 
 <template>
   <section class="page">
-    <h1 class="title">{{ title }}</h1>
     <slot></slot>
   </section>
 </template>
 
 <style scoped>
 @media (min-width: 1024px) {
-  .page {
-    color: var(--color-text);
-  }
 }
 </style>
---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <h1 class="title">{{ title }}</h1>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+}
+</style>
