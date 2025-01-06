b1688f2b7c53f19616664499c8f078d45f77a8ba
faruq <lekside1@gmail.com>
2025-01-06 03:21:41 -0500

    contact page content

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,66 @@
 <script setup lang="ts">
-import PageComponent from '@/components/PageComponent.vue'
+import TitleComponent from '@/components/TitleComponent.vue'
+import PageContent from '@/components/PageContent.vue'
+import IconsComponent from '@/components/IconsComponent.vue'
 </script>
 
 <template>
-  <PageComponent title="Contact" />
+  <PageContent class="contact">
+    <TitleComponent title="Contact" />
+
+    <!-- TODO: contact page content, icon svgs -->
+    <div class="contact-inner">
+      <div class="contact-item">
+        <IconsComponent icon="location" />
+        <div class="contact-item-text">
+          <span>Location:</span>
+          <span>Montreal, QC</span>
+        </div>
+      </div>
+
+      <div class="contact-item">
+        <a href="mailto:afaruqaliu@hotmail.com"><IconsComponent icon="email" /></a>
+        <div class="contact-item-text">
+          <span>Email:</span>
+          <span>afaruqaliu@hotmail.com</span>
+        </div>
+      </div>
+
+      <div class="contact-item">
+        <a href="tel:+1438887XXXX"><IconsComponent icon="phone" /></a>
+        <div class="contact-item-text">
+          <span>Phone:</span>
+          <span>+1 438 887 XXXX</span>
+        </div>
+      </div>
+    </div>
+  </PageContent>
 </template>
+
+<style scoped>
+.contact {
+  .contact-inner {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 2rem;
+    padding: 2rem 0;
+  }
+
+  .contact-item {
+    display: flex;
+    place-items: center;
+    gap: 0.5rem;
+  }
+
+  .contact-item-text {
+    display: grid;
+  }
+
+  @media (min-width: 1024px) {
+    .contact-inner {
+      flex-direction: row;
+    }
+  }
+}
+</style>
