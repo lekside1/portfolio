924b3402d9ffb6794cd8b3c7448c8466583e00c0
faruq <lekside1@gmail.com>
2025-07-21 18:34:27 -0400

    portfolio image added to items, item updated

--- src/components/PortfolioItem.vue
+++ src/components/PortfolioItem.vue
@@ -4,11 +4,23 @@ import IconsComponent from './IconsComponent.vue'
 defineProps<{
   name: string
   site: string
+  image?: string
 }>()
 </script>
 
 <template>
-  <div class="portfolio-item">
+  <div
+    class="portfolio-item"
+    :style="
+      image
+        ? {
+            backgroundImage: `url(${image})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }
+        : {}
+    "
+  >
     <a
       class="portfolio-item-link"
       :href="site"
@@ -24,21 +36,33 @@ defineProps<{
 
 <style scoped>
 .portfolio-item {
+  position: relative;
   border: 1px solid var(--color-text);
   border-radius: 1rem;
   box-shadow: 4px 4px var(--color-box-shadow);
   display: flex;
   place-content: center;
+  width: 100%;
+  min-width: 300px;
+  min-height: 250px;
 
   .portfolio-item-link {
+    width: 100%;
     display: flex;
-    align-items: center;
+    align-items: flex-end;
+    justify-content: flex-start;
     gap: 0.5rem;
     padding: 1rem;
+    color: var(--vt-c-white-soft);
   }
 
   .portfolio-item-name {
-    font-size: 1.2rem;
+    font-size: 1.3rem;
+  }
+
+  /* Mobile */
+  @media (max-width: 1023px) {
+    min-width: 200px;
   }
 
   /* Desktop */
--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -5,16 +5,41 @@ import TitleComponent from '@/components/TitleComponent.vue'
 import PageContent from '@/components/PageContent.vue'
 import PortfolioItem from '@/components/PortfolioItem.vue'
 
-// <!-- TODO: add portfolio item page content -->
+import marcelle from '@/assets/marcelle.png'
+import aib from '@/assets/aib.png'
+import watier from '@/assets/watier.png'
+import bentley from '@/assets/bentley.png'
+import molsoft from '@/assets/bentley.png'
+import lecompte from '@/assets/lecompte.png'
+import parmi from '@/assets/parmi.png'
+import gardeMalade from '@/assets/garde-malade.png'
+import maisonLavande from '@/assets/maison-lavande.png'
+import oberson from '@/assets/oberson.png'
+import naak from '@/assets/naak.png'
+import opus from '@/assets/opus.png'
+import unique from '@/assets/unique.png'
+import sportsExcellence from '@/assets/sports-excellence.png'
+import woodstock from '@/assets/woodstock.png'
+import holos from '@/assets/holos.png'
+
+// <!-- TODO: update portfolio item  -->
 const portfolioItems = ref([
-  { name: 'Molsoft', site: 'https://www.molsoft.io/' },
-  { name: 'Opus Art Supplies', site: 'https://opusartsupplies.com/' },
-  { name: 'Magasin LeCompte', site: 'https://maglecompte.ca/' },
-  { name: 'Oberson', site: 'https://oberson.com/' },
-  { name: 'Woodstock & cie', site: 'https://woodstockcie.com/' },
-  { name: 'Holos', site: 'https://www.liveholos.com/' },
-  { name: 'Unique Appliances', site: 'https://uniqueappliances.com/' },
-  { name: 'AIB International', site: 'https://aibinternational.com/' },
+  { name: 'Groupe Marcelle', site: 'https://marcelle.com/', image: marcelle },
+  { name: 'AIB International', site: 'https://aibinternational.com/', image: aib },
+  { name: 'Watier', site: 'https://watier.com/', image: watier },
+  { name: 'Bentley', site: 'https://www.shopbentley.com/', image: bentley },
+  { name: 'Molsoft', site: 'https://www.molsoft.io/', image: molsoft },
+  { name: 'Magasins LeCompte', site: 'https://maglecompte.ca/', image: lecompte },
+  { name: 'Parmi Lifewear', site: 'https://parmilifewear.com/', image: parmi },
+  { name: 'Garde Malade', site: 'https://www.garde-malade.ca/', image: gardeMalade },
+  { name: 'La Maison Lavande', site: 'https://maisonlavande.ca/', image: maisonLavande },
+  { name: 'Oberson', site: 'https://oberson.com/', image: oberson },
+  { name: 'Naak', site: 'https://www.naak.com/pages/nutrition-calculator', image: naak },
+  { name: 'Opus Art Supplies', site: 'https://opusartsupplies.com/', image: opus },
+  { name: 'Unique Appliances', site: 'https://uniqueappliances.com/', image: unique },
+  { name: 'Sports Excellence', site: 'https://sportsexcellence.com/', image: sportsExcellence },
+  { name: 'Woodstock & cie', site: 'https://woodstockcie.com/', image: woodstock },
+  { name: 'Holos', site: 'https://www.liveholos.com/', image: holos },
 ])
 </script>
 
@@ -28,6 +53,7 @@ const portfolioItems = ref([
         :key="item.name"
         :site="item.site"
         :name="item.name"
+        :image="item.image"
       />
     </div>
   </PageContent>
