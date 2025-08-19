110a2fca79c026e21458f0cb515e98eb3497fe8f
faruq <lekside1@gmail.com>
2025-08-19 04:08:54 -0400

    update my resume information

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -30,14 +30,15 @@ const workExpCardItems = ref([
       'Led a team of 3 developers across multiple projects; from design phase to completion, and ongoing maintenance.',
       'Developed custom Shopify themes and apps, ensuring responsive design and seamless user experience.',
       'Collaborated with product, design, and integration teams.',
-      'Troubleshoot and optimize websites to increase performance, SEO, and security.',
+      'Implemented and optimized internal tools, boosting productivity and reducing project completion time.',
+      'Troubleshoot and optimize websites to increase performance, SEO, accessibility and security.',
       'Performed unit testing, and wrote documentation to standardize project workflows and support team onboarding.',
       'Performed code reviews, ensuring adherence to coding standards',
       'Performed quality assurance to maintain high-quality output.',
       'Mentored junior developers, providing guidance on best practices and technical skills.',
     ],
     techStack:
-      'Javascript, Typescript, React, Shopify Liquid, Shopify Hydrogen, HTML, CSS/SCSS, REST API,  GraphQL, Node.js, Vanilla extract',
+      'Javascript, Typescript, React, Shopify Liquid, Shopify Hydrogen, HTML, CSS/SCSS, Tailwind, Vanilla extract, GraphQL, Node.js',
   },
   {
     title: 'Front-end Developer',
@@ -46,23 +47,23 @@ const workExpCardItems = ref([
     date: 'May 2021 - Mar 2023',
     description: [
       'Developed custom Shopify themes ensuring responsive design and seamless user experience across devices.',
-      'Troubleshoot and optimize websites to increase performance, SEO, and security.',
+      'Troubleshoot and optimize websites to increase performance, SEO, accessibility and security.',
       'Performed unit testing, and wrote documentation to standardize project workflows and support team onboarding.',
       'Performed code reviews, ensuring adherence to coding standards',
     ],
     techStack: 'Javascript, React, Shopify Liquid, HTML, CSS/SCSS',
   },
   {
-    title: 'Web Developer for CTCM - Intern',
-    company: 'CTCM (Corning Technology Center Montreal - iBwave Solutions)',
-    location: 'St-Laurent, Quebec',
+    title: 'Web Developer Intern - CTCM',
+    company: 'Corning Technology Center Montreal - iBwave Solutions',
+    location: 'Montreal, Quebec',
     date: 'Sept 2020 - Dec 2020',
     description: [
-      'Member of the DNA (Dense Network Architecture) Team.',
-      'Developed features for the DNA multi-connected network map.',
+      'Implemented features on an internal telecommunication network application',
+      'Shadowed developers to learn key details about job duties and tasks.',
     ],
     techStack:
-      'JavaScript, TypeScript, React, HTML, CSS, OpenLayers, PostgreSQL, C#/.Net, Microsoft Azure',
+      'JavaScript, TypeScript, React, HTML, CSS, OpenLayers, PostgreSQL, C#/.Net, Microsoft Azure services',
   },
   {
     title: 'Information Technology System Consultant - Intern',
@@ -70,20 +71,20 @@ const workExpCardItems = ref([
     location: 'Montreal, Quebec',
     date: 'May 2020 - Aug 2020',
     description: [
-      'Performed front-end development on the Wapikoni website.',
+      "Developed and maintained the organization's internal website improving user experience.",
       'Collaborated with various teams to assess organizational needs.',
-      'Proposed and implemented an optimized file storage and sharing solution, improving cross-team collaboration and accessibility of resources.',
+      'Implemented a new file-sharing system, improving team collaboration and workflow efficiency.',
     ],
     techStack: 'HTML, CSS, JavaScript, Google Drive',
   },
   {
-    title: 'Web Developer - Intern',
+    title: 'Web Developer Intern - Civil Aviation',
     company: 'CAE Inc.',
-    location: 'St-Laurent, Quebec',
+    location: 'Montreal, Quebec',
     date: 'Sept 2019 - Dec 2019',
     description: [
-      'Member of the Civil Aviation Courseware Development Team.',
-      'Front-end development on the Civil Aviation Courseware software.',
+      'Developed features on a civil aviation training courseware application.',
+      'Engaged in continuous learning of emerging technologies, incorporating industry advancements into projects when applicable.',
     ],
     techStack: 'JavaScript, React, Vue.js, HTML, CSS, PouchDB/CouchDB',
   },
@@ -91,17 +92,28 @@ const workExpCardItems = ref([
 
 const educationCardItems = ref([
   {
-    title: 'Bachelor of Computer Science – General Program Co-op',
+    title: 'Bachelor of Computer Science - Co-op',
     school: 'Gina Cody School of Engineering and Computer Science, Concordia University',
     location: 'Montreal, Quebec',
     date: '2018 - 2021',
     description: ['Member of the Institute for Co-operative Education'],
   },
   {
-    title: 'Bachelor of Science – Specialization in Biology',
+    title: 'Bachelor of Science - Specialization in Biology',
     school: 'Concordia University',
     location: 'Montreal, Quebec',
     date: '2013 - 2016',
+  },
+  {
+    title: 'Certifications',
+    school: 'LinkedIn Learning',
+    date: '2019',
+    description: [
+      'Node.js Essential Training (Nov 2019)',
+      'React.js Essential Training (Nov 2019)',
+      'Learning Backbone.js (Oct 2019)',
+      'Responsive Layout (Oct 2019)',
+    ],
   },
 ])
 </script>
@@ -138,36 +150,34 @@ const educationCardItems = ref([
       <div class="tab-content">
         <div v-if="selectedTab === 0">
           <div class="tab-content-item">
-            <h2 class="green">Professional Summary</h2>
+            <h2 class="green uppercase">Professional Summary</h2>
             <p>
-              <strong>Detail-oriented Software Developer</strong> with over 4 years of experience
-              and a proven ability to collaborate in teams, communicate effectively, and efficiently
-              resolve complex technical challenges. A quick learner with a
-              <strong>results-driven mindset</strong>, committed to delivering
-              <strong>high-quality, innovative solutions</strong>. Proficient in
-              <strong>modern development frameworks, languages, and tools</strong>, with expertise
-              in
-              <strong
-                >React, TypeScript, Shopify Liquid, Node.js, GraphQL, and SQL/NoSQL
-                databases</strong
-              >, ensuring scalable and robust software delivery.
+              Detail-oriented Software Developer with 4+ years of experience in web development,
+              creating interactive, user-centered scalable web applications. Proficient in modern
+              front-end and back-end technologies, ensuring high performance, clean and maintainable
+              code. Strong problem-solver with a focus on seamless user experience and performance
+              optimization. A collaborative team player with the ability to effectively translate
+              business goals into robust technical solutions.
             </p>
           </div>
 
           <div class="tab-content-item">
-            <h2 class="green">Summary of Skills</h2>
+            <h2 class="green uppercase">Skills</h2>
             <ul>
               <li>HTML, CSS/SCSS, JavaScript, TypeScript</li>
-              <li>React.js, Vue.js, Shopify Liquid</li>
+              <li>React.js, Vue.js, Backbone.js, Shopify Liquid</li>
               <li>Node.js, Python, Java, C/C++, C#/.NET</li>
 
               <li>SQL/NoSQL Databases</li>
-              <li>RESTful APIs, GraphQL</li>
+              <li>RESTful APIs, GraphQL, Microsoft Azure</li>
 
               <li>Git, GitHub, GitLab</li>
               <li>Agile, Scrum, Kanban</li>
 
-              <li>Shopify Plus, Shopify Hydrogen, Figma, Jira, Confluence, Storybook</li>
+              <li>
+                Shopify Plus, Shopify Hydrogen, VS Code, Eclipse, Figma, Jira, Confluence, Storybook
+              </li>
+              <li>Code Refactoring, Debugging, Testing, Performance Optimization, Documentation</li>
             </ul>
           </div>
         </div>
@@ -250,6 +260,12 @@ const educationCardItems = ref([
 
       .button-title {
         display: block;
+      }
+    }
+
+    .button-download {
+      .button-title {
+        font-size: 13.333333px;
       }
     }
 
